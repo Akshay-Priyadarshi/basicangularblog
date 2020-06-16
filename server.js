568da77efe99800f555d835afef7b46d5dda0f8f
Akshay-Priyadarshi <akshayp1904@outlook.com>
2020-06-16 22:31:57 +0530

    Third commit

--- server.js
+++ server.js
@@ -28,13 +28,18 @@ if (process.env.NODE_ENV === "production") {
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "public/index.html"));
   });
+} else {
+  app.use(express.static(path.join(__dirname, "public")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+  });
 }
+app.use(express.static(path.join(__dirname, "public")));
 
-// app.use(express.static(path.join(__dirname, "public")));
-
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "public/index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
 
 app.listen(port, () => {
   console.log(`Server started at port ${port}`);
